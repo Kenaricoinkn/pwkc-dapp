94e99c6adb278db983bac91484b550d3737a20cc
Kenaricoin <kenaricoinkn@gmail.com>
2025-09-05 23:38:42 +0700

    Delete src directory

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { HashRouter } from "react-router-dom";
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
