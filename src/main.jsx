567d78fef0921af26c055a38b8b232f194d0a32d
Kenaricoin <kenaricoinkn@gmail.com>
2025-09-05 23:34:05 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
